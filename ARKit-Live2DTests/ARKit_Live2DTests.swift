bcda87a96b07240d35431772c94abf9495d088f4
Yuki MIZUNO <mizuyuu0904@gmail.com>
2017-11-14 02:17:31 +0900

    Initial Commit

---
+++ ARKit-Live2DTests/ARKit_Live2DTests.swift
@@ -0,0 +1,36 @@
+//
+//  ARKit_Live2DTests.swift
+//  ARKit-Live2DTests
+//
+//  Created by Yuki MIZUNO on 2017/11/14.
+//  Copyright © 2017年 Yuki MIZUNO. All rights reserved.
+//
+
+import XCTest
+@testable import ARKit_Live2D
+
+class ARKit_Live2DTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
