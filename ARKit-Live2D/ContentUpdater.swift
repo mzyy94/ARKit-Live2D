9653a0969deffd4db86c8235363fc373f562898f
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-02-24 04:43:42 +0900

    Add more parameters

--- ARKit-Live2D/ContentUpdater.swift
+++ ARKit-Live2D/ContentUpdater.swift
@@ -57,7 +57,8 @@ class ContentUpdater: NSObject, ARSCNViewDelegate {
             let browOuterUpLeft = faceAnchor.blendShapes[.browOuterUpLeft] as? Float,
             let browOuterUpRight = faceAnchor.blendShapes[.browOuterUpRight] as? Float,
             let mouthFunnel = faceAnchor.blendShapes[.mouthFunnel] as? Float,
-            let jawOpen = faceAnchor.blendShapes[.jawOpen] as? Float
+            let jawOpen = faceAnchor.blendShapes[.jawOpen] as? Float,
+            let cheekPuff = faceAnchor.blendShapes[.cheekPuff] as? Float
             else { return }
         
         let newFaceMatrix = SCNMatrix4.init(faceAnchor.transform)
@@ -67,21 +68,25 @@ class ContentUpdater: NSObject, ARSCNViewDelegate {
         live2DModel.setParam("ParamAngleY", value: faceNode.eulerAngles.x * -360 / Float.pi)
         live2DModel.setParam("ParamAngleX", value: faceNode.eulerAngles.y * 360 / Float.pi)
         live2DModel.setParam("ParamAngleZ", value: faceNode.eulerAngles.z * -360 / Float.pi)
-
+        
+        live2DModel.setParam("ParamBodyAngleX", value: faceNode.position.z * 20)
+        live2DModel.setParam("ParamBodyAngleY", value: faceNode.position.y * 20)
+        
         live2DModel.setParam("ParamEyeBallX", value: faceAnchor.lookAtPoint.x * 2)
         live2DModel.setParam("ParamEyeBallY", value: faceAnchor.lookAtPoint.y * 2)
         
         live2DModel.setParam("ParamBrowLY", value: -(0.5 - browOuterUpLeft))
         live2DModel.setParam("ParamBrowRY", value: -(0.5 - browOuterUpRight))
-        live2DModel.setParam("ParamBrowLAngle", value: 2*(browInnerUp - browOuterUpLeft))
-        live2DModel.setParam("ParamBrowRAngle", value: 2*(browInnerUp - browOuterUpRight))
+        live2DModel.setParam("ParamBrowLAngle", value: 16*(browInnerUp - browOuterUpLeft) - 1.6)
+        live2DModel.setParam("ParamBrowRAngle", value: 16*(browInnerUp - browOuterUpRight) - 1.6)
 
         live2DModel.setParam("ParamEyeLOpen", value: 1.0 - eyeBlinkLeft)
         live2DModel.setParam("ParamEyeROpen", value: 1.0 - eyeBlinkRight)
         
         live2DModel.setParam("ParamMouthOpenY", value: jawOpen*1.8)
         live2DModel.setParam("ParamMouthForm", value: 1 - mouthFunnel*2)
-
+        
+        live2DModel.setParam("ParamCheek", value: cheekPuff);
     }
 }
 
